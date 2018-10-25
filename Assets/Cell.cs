0969de5edf0f70300df66e173ae015a29e087f60
Adam <adam.torngren@hotmail.com>
2018-10-25 10:16:26 +0200

    Initial commit

---
+++ Assets/Cell.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell {
+
+    public int x, y;
+    public Color cellColor;
+    public bool alive;
+
+    public Cell(int x, int y, Color cellColor, bool alive) {
+        this.x = x;
+        this.y = y;
+        this.cellColor = cellColor;
+        this.alive = alive;
+    }
+
+    public Cell(Cell cell) {
+        this.x = cell.x;
+        this.y = cell.y;
+        this.cellColor = cell.cellColor;
+        this.alive = cell.alive;
+    }
+
+	public void setState(bool alive) {
+        this.alive = alive;
+    }
+
+    public static bool Equals(Cell obj1, Cell obj2)
+    {
+        return obj1 == obj2;
+    }
+
+    public static bool operator ==(Cell obj1, Cell obj2){
+        if (ReferenceEquals(obj1, obj2)) {
+            return true;
+        }
+
+        if (ReferenceEquals(obj1, null)) {
+            return false;
+        }
+
+        if (ReferenceEquals(obj2, null)) {
+            return false;
+        }
+
+        return (obj1.x == obj2.x
+                && obj1.y == obj2.y
+                && obj1.alive == obj2.alive);
+    }
+
+    // this is second one '!='
+    public static bool operator !=(Cell obj1, Cell obj2) {
+        return !(obj1 == obj2);
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float chanceOfMutation;
+    public float fillPercentage;
+    public int cellsInRow;
+    public int cellsInColumn;
+
+
+    public float tick;
+    float nextTick = 0f;
+
+    public GameObject cell;
+    Simulation simulation;
+
+    GameObject[] cellObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        simulation = new Simulation(this);
+        cellObjects = new GameObject[this.cellsInRow * this.cellsInColumn];
+
+        for (int y = 0; y < this.cellsInRow; ++y)
+            for (int x = 0; x < this.cellsInColumn; ++x) {
+                Vector3 spawnOffset = new Vector3(x + 1, 0, y);
+                cellObjects[x + this.cellsInRow * y] = Instantiate(cell, transform.position + spawnOffset, transform.rotation, transform);
+
+                if (Random.Range(0, 100) > (this.fillPercentage * 100)) {
+                    simulation.setCell(x, y, false);
+                    cellObjects[x + this.cellsInRow * y].GetComponent<MeshRenderer>().enabled = false;
+                }
+                else {
+                    simulation.setCell(x, y, true);
+                }
+            }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (this.nextTick > this.tick)
+        {
+            this.setCellRendering(this.simulation.cells);
+            simulation.step();
+            this.nextTick -= this.tick;
+        }
+        this.nextTick += Time.deltaTime;
+    }
+
+    void setCellRendering(Cell[] newCells)
+    {
+        for (int i = 0; i < newCells.Length; i++)
+        {
+            if (newCells[i].alive)
+            {
+                cellObjects[i].GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                cellObjects[i].GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+}
+
